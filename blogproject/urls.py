07df9bd1dc3fea5412b8c0ff8cc9d52359632611
techofficer20 <48676268+techofficer20@users.noreply.github.com>
2019-07-14 20:24:44 +0900

    heroku install

---
+++ blogproject/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+import blogapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', blogapp.views.index, name='index'),
+]
